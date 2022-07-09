be7d78ed97279912b0a332b9793cbcfd4ff1c988
Augusto Schiapati Borges <augusto.schiapati@gmail.com>
2022-07-09 18:39:12 -0300

    All files

---
+++ ExternalApplication.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.UI;
+using System.Reflection;
+using System.Windows.Media.Imaging;
+
+namespace MyRevitCommands
+{
+    class ExternalApplication : IExternalApplication
+    {
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+        public Result OnStartup(UIControlledApplication application)
+        {
+            //Create Ribbon Tab
+            application.CreateRibbonTab("My Commands");
+
+            string path = Assembly.GetExecutingAssembly().Location;
+            PushButtonData button = new PushButtonData("Button1", "PlaceFamily", path, "MyRevitCommands.PlaceFamily");
+
+            RibbonPanel panel = application.CreateRibbonPanel("My Commands", "Commands");
+
+            //Add button image
+            Uri imagePath = new Uri(@"C:\Users\aschi\Downloads\desk.png");
+            //BitmapImage image = new BitmapImage(imagePath);
+
+            PushButton pushButton = panel.AddItem(button) as PushButton;
+            //pushButton.LargeImage = image;
+            
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ My Commands/Tests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace MyRevitCommands
+{
+    [TransactionAttribute(TransactionMode.Manual)]
+    [RegenerationAttribute(RegenerationOption.Manual)]
+    class Tests : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+
+            UIApplication app = commandData.Application;
+            UIDocument uidoc = app.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+            RevitLinkInstance rli = new FilteredElementCollector(doc)
+                .WhereElementIsNotElementType()
+                .OfCategory(BuiltInCategory.OST_RvtLinks)
+                .OfType<RevitLinkInstance>()
+                .Cast<RevitLinkInstance>()
+                .First();
+
+            Document docLink = rli.GetLinkDocument();
+
+            List<Wall> walls = new FilteredElementCollector(docLink)
+                .WhereElementIsNotElementType()
+                .OfCategory(BuiltInCategory.OST_Walls)
+                .Cast<Wall>()
+                .ToList();
+
+            List<GeometryElement> wallGeos = walls.Select(wall => wall.get_Geometry(new Options())).ToList();
+            List<Solid> wallSolids = wallGeos.Select(solid => solid.OfType<Solid>().Cast<Solid>().Single()).ToList();
+            Solid uniqueSolid = null;
+            foreach(Solid solid in wallSolids)
+            {
+                if (uniqueSolid is null) uniqueSolid = solid;
+                uniqueSolid = BooleanOperationsUtils
+                  .ExecuteBooleanOperation(uniqueSolid, solid,
+                    BooleanOperationsType.Union);
+            }
+
+            Transform Trli = rli.GetTotalTransform();
+            Solid TSolid = SolidUtils.CreateTransformed(uniqueSolid, Trli);
+            //CRIAR OS SOLIDOS TRANSFORMADOS E JUNTAR, DEPOIS CONTINUAR O CÓDIGO... \/
+            PlanarFace inferiorFace = TSolid.Faces.Cast<PlanarFace>().Single(face => Math.Abs(face.FaceNormal.Z + 1) < 0.01);
+            CurveLoop externalCurve = inferiorFace.GetEdgesAsCurveLoops().OrderByDescending(curveLoop => curveLoop.GetExactLength()).First();
+            CurveLoop middleCurve = CurveLoop.CreateViaOffset(externalCurve, UnitUtils.ConvertToInternalUnits(10, UnitTypeId.Centimeters), XYZ.BasisZ);
+
+            Level level = new FilteredElementCollector(doc)
+                .WhereElementIsNotElementType()
+                .OfCategory(BuiltInCategory.OST_Levels)
+                .Cast<Level>()
+                .OrderByDescending(height => height.Elevation)
+                .First();
+
+            using (Transaction createWalls = new Transaction(doc, "New Walls"))
+            {
+                createWalls.Start();
+                middleCurve.ToList().ForEach(curve => Wall.Create(doc, curve, level.Id, false));
+                createWalls.Commit();
+            }
+            return Result.Succeeded;
+        }
+    }
+}
